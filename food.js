c044d3d09df942a5d53e3084019d17afc6b19ab3
as0628 <anshusingh0284087>
2025-06-03 17:50:20 +0530

    Your commit message

---
+++ food.js
@@ -0,0 +1,14 @@
+const mainHeading = document.getElementById("main-heading")
+mainHeading.textConent = "Fruit World"
+mainHeading.style.color = "orange"
+const header = document.getElementById("header")
+header.style.backgroundColor = "green"
+header.style.borderbottom = "4px solid orange"
+const basketHeading = document.getElementById("basket-handing")
+basketHeading.style.color = "green"
+const thanksDiv = document.getElementById("thanks")
+const para = document.createElement("p")
+para.textConent = "Please visit us again"
+thanksDiv.appendChild(para)
+
+
